99ae5f8bcf84052395a090b97efe82d4f867ec73
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-06-01 09:06:32 +0200

    Add files via upload

---
+++ src/sqlconnector/Constant.java
@@ -0,0 +1,16 @@
+package sqlconnector;
+
+
+// erstat konstanterne nedenfor
+
+public abstract class Constant
+{
+	public static final String
+		server					= "localhost",  // database-serveren
+		database				= "20_weightdb",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "root", // dit brugernavn = dit studienummer 
+		password				= ""; // dit password som du har valgt til din database
+	
+	public static final int
+		port					= 3306;
+}
