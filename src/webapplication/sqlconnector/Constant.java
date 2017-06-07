4710b999715699f1bdecb60abf7e2d20d51becb5
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-06-07 10:06:21 +0200

    New Database THAT WORKS

--- src/webapplication/sqlconnector/Constant.java
+++ src/webapplication/sqlconnector/Constant.java
@@ -6,10 +6,10 @@ package webapplication.sqlconnector;
 public abstract class Constant
 {
 	public static final String
-		server					= "13.58.160.148",  	// database-serveren
-		database				= "20cdioFinal",  			//"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "cdio", 					// dit brugernavn = dit studienummer 
-		password				= "cdio_admin"; 			// dit password som du har valgt til din database
+		server					= "eu-cdbr-west-01.cleardb.com",  	// database-serveren
+		database				= "heroku_64a70c889854da5",  			//"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "b59d0a775e066f", 					// dit brugernavn = dit studienummer 
+		password				= "fac2ad55"; 			// dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
