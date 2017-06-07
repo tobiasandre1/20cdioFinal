12b9f258ed3942cf40fcdc9fec570b29b1d1d833
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-06-07 12:15:41 +0200

    Removed roles from Operatoer Implementation

--- src/webapplication/datalayer/MySQLOperatoerDAO.java
+++ src/webapplication/datalayer/MySQLOperatoerDAO.java
@@ -27,16 +27,9 @@ public class MySQLOperatoerDAO implements OperatoerDAO {
 		
 		String[] values = new String[]{Integer.toString(oprId)};
 		
-		//String rolestatement = map.getStatement("ro_SELECT_ALL_opr_id");
 		String statement = map.getStatement("opr_SELECT");
-		
-		//rolestatement = map.insertValuesIntoString(rolestatement, values);
 		statement = map.insertValuesIntoString(statement, values);
-		
-		//System.out.println("Query: "+rolestatement);
 		System.out.println("Query: "+statement);
-		
-		//ResultSet role_rs = Connector.doQuery(rolestatement);
 		ResultSet rs = Connector.doQuery(statement);
 		
 		OperatoerDTO temp;
@@ -45,18 +38,7 @@ public class MySQLOperatoerDAO implements OperatoerDAO {
 	    	//Get the operatoer
 	    	if (!rs.first()) throw new DALException("Operatoeren " + oprId + " findes ikke");
 	    	temp = new OperatoerDTO (rs.getInt("opr_id"), rs.getString("opr_navn"), rs.getString("ini"), rs.getString("password"), rs.getBoolean("opr_active"));
-	    	
-	    	/*
-	    	//Get the roles
-	    	try{
-    			List<String> roles = new ArrayList<String>();
-    			while (role_rs.next()){
-    				roles.add(role_rs.getString("role"));
-    			}
-    			temp.setRoles(roles);
-    			return temp;
-    		}
-    		catch(SQLException e){System.out.println("Error fetching roles" + e);throw new DALException(e);}*/
+	    	//TODO manage roles
 	    	return temp;
 	    }
 	    catch (SQLException e) {throw new DALException(e); }
@@ -67,36 +49,15 @@ public class MySQLOperatoerDAO implements OperatoerDAO {
 	public List<OperatoerDTO> getOperatoerList() throws DALException {
 		List<OperatoerDTO> list = new ArrayList<OperatoerDTO>();
 		
-		//String rolestatement; 
-		//String[] values; 
-		
 		ResultSet rs = Connector.doQuery(map.getStatement("opr_SELECT_ALL"));
-		//ResultSet role_rs;
+		
 		OperatoerDTO temp;
 		try
 		{
 			while (rs.next()) 
 			{
 				temp = new OperatoerDTO(rs.getInt("opr_id"), rs.getString("opr_navn"), rs.getString("ini"), rs.getString("password"), rs.getBoolean("opr_active"));
-				System.out.println(temp);
-				/*
-		    	try{
-		    		//Preparing role fetch statement
-		    		rolestatement = map.getStatement("ro_SELECT_opr_id");
-		    		values = new String[]{Integer.toString(temp.getOprId())};
-		    		rolestatement = map.insertValuesIntoString(rolestatement, values);
-		    		role_rs = Connector.doQuery("ro_SELECT_opr_id");
-		    		
-		    		//Fetching roles
-		    		List<String> roles = new ArrayList<String>();
-		    		while (role_rs.next()){
-		    			roles.add(role_rs.getString("role"));
-		    		}
-		    			
-		    		temp.setRoles(roles);
-		     	}
-		    	catch(SQLException e){System.out.println("Error fetching roles" + e);throw new DALException(e);}*/
-				
+				//System.out.println(temp);
 				list.add(temp);
 			}
 		}
@@ -130,22 +91,6 @@ public class MySQLOperatoerDAO implements OperatoerDAO {
 		statement = map.insertValuesIntoString(statement, values);
 		System.out.println(statement);
 		Connector.doUpdate(statement);
-		
-		/*
-		//Delete the roles the operatoer has already
-		statement = map.getStatement("ro_DELETE");
-		values = new String[]{Integer.toString(opr.getOprId())};
-		statement = map.insertValuesIntoString(statement, values);
-		Connector.doUpdate(statement);
-		
-		//Give the operatoer new roles
-		List<String> roles = opr.getRoles();
-		for(String role : roles){
-			statement = map.getStatement("ro_INSERT");
-			values = new String[]{Integer.toString(opr.getOprId()), role};
-			statement = map.insertValuesIntoString(statement, values);
-			Connector.doUpdate(statement);
-		}*/
 
 	}
 
