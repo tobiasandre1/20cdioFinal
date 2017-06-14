a4a85504d4fe7cb86ddee487bc7c00dc239aad12
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-06-14 11:49:16 +0200

    Variables are not longer stored in other dto

--- src/webapplication/datatransferobjects/ViewUdskriftDTO.java
+++ src/webapplication/datatransferobjects/ViewUdskriftDTO.java
@@ -3,11 +3,18 @@ package webapplication.datatransferobjects;
 public class ViewUdskriftDTO {
 
 	private ProduktBatchKompDTO pbkomp;
+	
 	private String oprNavn;
 	private int raavareId;
 	private String raavareNavn;
 	private int receptId;
 	private int status;
+	
+	int pbId; 	  // produktbatchets id
+	int rbId;        // i omraadet 1-99999999
+	double tara;
+	double netto;
+	int oprId;					// operatoer-nummer
 	
 	public ViewUdskriftDTO(ProduktBatchKompDTO pbkomp, String oprNavn, int raavareId, String raavareNavn, int receptId,
 			int status) {
@@ -18,6 +25,12 @@ public class ViewUdskriftDTO {
 		this.raavareNavn = raavareNavn;
 		this.receptId = receptId;
 		this.status = status;
+		
+		this.pbId = pbkomp.getPbId();
+		this.rbId = pbkomp.getPbId();
+		this.tara = pbkomp.getTara();
+		this.netto = pbkomp.getNetto();
+		this.oprId = pbkomp.getOprId();
 	}
 
 	public ProduktBatchKompDTO getPbkomp() {
@@ -71,5 +84,45 @@ public class ViewUdskriftDTO {
 	@Override
 	public String toString(){
 		return pbkomp + "\t" + oprNavn + "\t" + raavareId + "\t" + raavareNavn + "\t" + receptId + "\t" + status;
+	}
+
+	public int getPbId() {
+		return pbId;
+	}
+
+	public void setPbId(int pbId) {
+		this.pbId = pbId;
+	}
+
+	public int getRbId() {
+		return rbId;
+	}
+
+	public void setRbId(int rbId) {
+		this.rbId = rbId;
+	}
+
+	public double getTara() {
+		return tara;
+	}
+
+	public void setTara(double tara) {
+		this.tara = tara;
+	}
+
+	public double getNetto() {
+		return netto;
+	}
+
+	public void setNetto(double netto) {
+		this.netto = netto;
+	}
+
+	public int getOprId() {
+		return oprId;
+	}
+
+	public void setOprId(int oprId) {
+		this.oprId = oprId;
 	}
 }
